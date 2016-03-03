245dbbbce67a5be98911cfa8e5cb11c8f6fca31d
liuqinglin <liuqinglin@cd.tuan800.com>
2016-03-03 16:27:39 +0800

    add bootstrap

--- app/helpers/web_sql/application_helper.rb
+++ app/helpers/web_sql/application_helper.rb
@@ -1,4 +1,11 @@
 module WebSql
   module ApplicationHelper
+    def format_time(string)
+      begin
+        DateTime.parse(string).strftime("%Y-%m-%d %H:%M:%S")
+      rescue 
+        string
+      end
+    end
   end
 end
--- lib/web_sql.rb
+++ lib/web_sql.rb
@@ -1,5 +1,6 @@
 require "web_sql/model"
 require "web_sql/engine"
+require "bootstrap-sass"
 
 module WebSql
 end
